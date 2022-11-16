b10564dbc035ce06a6cf73f67b7e9040f316e2f5
oberdancunha <oberdancunha@gmail.com>
2022-11-16 00:49:22 -0300

    Windows platform scaffoldBackgroundColor white.

--- fluttemis_platform_ui_design_system/lib/presentation/components/platform/theme/light/windows_theme_data_light.dart
+++ fluttemis_platform_ui_design_system/lib/presentation/components/platform/theme/light/windows_theme_data_light.dart
@@ -2,5 +2,6 @@ import 'package:fluent_ui/fluent_ui.dart';
 
 final windowsThemeDataLight = ThemeData().copyWith(
   brightness: Brightness.light,
+  scaffoldBackgroundColor: Colors.white,
   cardColor: const Color.fromRGBO(156, 39, 176, 1),
 );
