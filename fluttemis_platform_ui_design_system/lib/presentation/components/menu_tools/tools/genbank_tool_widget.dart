08c1591d50b213a6f35f50c1084b9de0548a087f
oberdancunha <oberdancunha@gmail.com>
2022-11-18 15:41:55 -0300

    Genbank menu tool widget.

---
+++ fluttemis_platform_ui_design_system/lib/presentation/components/menu_tools/tools/genbank_tool_widget.dart
@@ -0,0 +1,31 @@
+import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../platform/tool_card/platform_tool_card_widget.dart';
+
+class GenbankToolWidget extends StatefulWidget {
+  const GenbankToolWidget({super.key});
+
+  @override
+  State<GenbankToolWidget> createState() => _GenbankToolWidgetState();
+}
+
+class _GenbankToolWidgetState extends State<GenbankToolWidget> {
+  late List<String> genbankExtensionsAccepted;
+
+  @override
+  void initState() {
+    super.initState();
+    genbankExtensionsAccepted = ['genbank', 'gbk', 'gb', 'gbff'];
+  }
+
+  @override
+  Widget build(BuildContext context) => PlatformToolCardWidget(
+        image: 'assets/images/ncbi.png',
+        mainTitle: 'Genbank',
+        secondaryTitle: FluttemisAppLocalizations.of(context)!.openFile,
+        description: FluttemisAppLocalizations.of(context)!.genbankDescription,
+        mainDescriptionExtend: genbankExtensionsAccepted.join(', '),
+        secondaryDescriptionExtend: FluttemisAppLocalizations.of(context)!.toolAcceptedExtensions,
+      );
+}
