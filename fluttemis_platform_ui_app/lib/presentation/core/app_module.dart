f62efa0b5b237dc51142095c8f3f6d6b93c4b4f1
oberdancunha <oberdancunha@gmail.com>
2022-11-22 11:21:19 -0300

    Genbank module route navigation.

--- fluttemis_platform_ui_app/lib/presentation/core/app_module.dart
+++ fluttemis_platform_ui_app/lib/presentation/core/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
 
 import '../home/home_page.dart';
+import '../modules/genbank_module.dart';
 
 class AppModule extends Module {
   @override
@@ -11,6 +12,10 @@ class AppModule extends Module {
     ChildRoute(
       '/',
       child: (_, __) => const HomePage(),
+    ),
+    ModuleRoute(
+      '/genbank/',
+      module: GenbankModule(),
     ),
   ];
 }
