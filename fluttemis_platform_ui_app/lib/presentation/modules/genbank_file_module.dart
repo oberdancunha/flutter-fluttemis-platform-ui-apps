9e7e9129c75e01b6f07733dd570fc5fbf65ee71a
oberdancunha <oberdancunha@gmail.com>
2022-11-27 23:01:18 -0300

    Change genbank_module to genbank_file_module.

---
+++ fluttemis_platform_ui_app/lib/presentation/modules/genbank_file_module.dart
@@ -0,0 +1,32 @@
+import 'package:fluttemis_platform_ui_core/application/locus/locus_store.dart';
+import 'package:fluttemis_platform_ui_core/configurations/locus_module_binds.dart';
+import 'package:fluttemis_platform_ui_core/external/locus/locus_data_source_genbank_file.dart';
+import 'package:fluttemis_platform_ui_core/infrastructure/locus/i_locus_data_source.dart';
+import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
+
+import '../locus_map/locus_map_page.dart';
+
+class GenbankFileModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.factory<ILocusDataSource>(
+      (i) {
+        final genbankFile = i.args.data.toString();
+
+        return LocusDataSourceGenbankFile(genbankFile: genbankFile);
+      },
+    ),
+    ...locusModuleBinds,
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(
+      '/',
+      child: (_, args) => LocusMapPage(
+        locusStore: Modular.get<LocusStore>(),
+      ),
+      transition: TransitionType.fadeIn,
+    ),
+  ];
+}
