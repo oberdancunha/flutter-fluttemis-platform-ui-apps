f3f1c086a6d5a89533ddc297377a2a69434138d9
oberdancunha <oberdancunha@gmail.com>
2022-11-27 23:00:02 -0300

    Change genbank_module_binds name to locus_module_binds.

--- fluttemis_platform_ui_core/lib/configurations/locus_module_binds.dart
+++ fluttemis_platform_ui_core/lib/configurations/locus_module_binds.dart
@@ -2,19 +2,11 @@ import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_de
 
 import '../application/locus/locus_store.dart';
 import '../domain/locus/i_locus_repository.dart';
-import '../external/locus/locus_data_source_genbank_file.dart';
 import '../infrastructure/locus/i_locus_data_source.dart';
 import '../infrastructure/locus/locus_overlap.dart';
 import '../infrastructure/locus/locus_repository.dart';
 
-List<Bind<Object>> get genbankModuleBinds => [
-      Bind.factory<ILocusDataSource>(
-        (i) {
-          final genbankFile = i.args.data.toString();
-
-          return LocusDataSourceGenbankFile(genbankFile: genbankFile);
-        },
-      ),
+List<Bind<Object>> get locusModuleBinds => [
       Bind.factory<ILocusRepository>(
         (i) => LocusRepository(
           locusDataSource: i<ILocusDataSource>(),
