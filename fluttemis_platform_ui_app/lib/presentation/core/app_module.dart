1f77234c7350c2fcd04e2470558428d14aab16de
oberdancunha <oberdancunha@gmail.com>
2022-11-30 01:17:04 -0300

    Add TransitionType.fadeIn to the initial route access.

--- fluttemis_platform_ui_app/lib/presentation/core/app_module.dart
+++ fluttemis_platform_ui_app/lib/presentation/core/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:fluttemis_platform_ui_core/driver/file_picker_impl.dart';
 import 'package:fluttemis_platform_ui_core/driver/i_file_picker.dart';
+import 'package:fluttemis_platform_ui_core/store/access_history/access_history_store.dart';
 import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
 
 import '../home/home_page.dart';
@@ -9,6 +10,7 @@ class AppModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton<IFilePicker>((_) => FilePickerImpl()),
+    Bind.lazySingleton((_) => AccessHistoryStore()),
   ];
 
   @override
@@ -16,6 +18,7 @@ class AppModule extends Module {
     ChildRoute(
       '/',
       child: (_, __) => const HomePage(),
+      transition: TransitionType.fadeIn,
     ),
     ModuleRoute(
       '/genbankfile/',
