aed2e711a55d5d8f95c6f2c22833c1561ff5bb15
oberdancunha <oberdancunha@gmail.com>
2022-12-02 00:03:34 -0300

    Path pattern to windows.

--- fluttemis_platform_ui_core/lib/utils/utils.dart
+++ fluttemis_platform_ui_core/lib/utils/utils.dart
@@ -1,5 +1,5 @@
 String getFileNameFromPath(String filePath) {
-  const pathPattern = '/';
+  final pathPattern = RegExp(r'[\/\\]');
   if (filePath.contains(pathPattern)) {
     final filePathSplitted = filePath.split(pathPattern);
     final fileName = filePathSplitted.last;
