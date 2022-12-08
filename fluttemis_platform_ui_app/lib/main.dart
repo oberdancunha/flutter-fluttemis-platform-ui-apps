98f836d89231d5313d1135bf8ba85cc61446ae40
oberdancunha <oberdancunha@gmail.com>
2022-12-08 11:48:46 -0300

    Remove backgroundColor and material import.

--- fluttemis_platform_ui_app/lib/main.dart
+++ fluttemis_platform_ui_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'presentation/core/app_module.dart';
 import 'presentation/core/app_widget.dart';
@@ -10,7 +10,6 @@ Future<void> main() async {
   const WindowOptions windowOptions = WindowOptions(
     minimumSize: Size(512, 384),
     center: true,
-    backgroundColor: Colors.transparent,
     skipTaskbar: false,
     titleBarStyle: TitleBarStyle.normal,
   );
