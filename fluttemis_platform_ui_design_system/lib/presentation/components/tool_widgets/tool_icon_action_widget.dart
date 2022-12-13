ad997fcba73256615405709ac277ccdfb020b5ab
oberdancunha <oberdancunha@gmail.com>
2022-12-12 21:28:08 -0300

    Change InkWell to GestureDetector.

--- fluttemis_platform_ui_design_system/lib/presentation/components/tool_widgets/tool_icon_action_widget.dart
+++ fluttemis_platform_ui_design_system/lib/presentation/components/tool_widgets/tool_icon_action_widget.dart
@@ -1,32 +1,29 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+import '../platform/tool_icon/platform_tool_icon_widget.dart';
 import '../platform/tooltip/platform_tooltip_widget.dart';
-import 'tool_icon_widget.dart';
 
 class ToolIconActionWidget extends StatelessWidget {
   final String image;
-  final Color imageColor;
   final double width;
   final String message;
-  final VoidCallback action;
+  final VoidCallback toolAction;
 
   const ToolIconActionWidget({
     required this.image,
-    required this.imageColor,
     required this.width,
     required this.message,
-    required this.action,
+    required this.toolAction,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) => PlatformTooltipWidget(
         message: message,
-        child: InkWell(
-          onTap: action,
-          child: ToolIconWidget(
+        child: GestureDetector(
+          onTap: toolAction,
+          child: PlatformToolIconWidget(
             image: image,
-            imageColor: imageColor,
             width: width,
           ),
         ),
