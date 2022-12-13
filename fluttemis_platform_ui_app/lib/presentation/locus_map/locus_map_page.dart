007d8c28f5e3d66b2cdb2b561d614853b1f564f9
oberdancunha <oberdancunha@gmail.com>
2022-12-13 15:14:18 -0300

    Create locus_map_page file.

---
+++ fluttemis_platform_ui_app/lib/presentation/locus_map/locus_map_page.dart
@@ -0,0 +1,24 @@
+import 'package:fluttemis_platform_ui_core/domain/locus/locus.dart';
+import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
+import 'package:fluttemis_platform_ui_design_system/presentation/components/top_menu_tools/top_menu_tools_widget.dart';
+import 'package:flutter/widgets.dart';
+
+class LocusMapPage extends StatelessWidget {
+  final KtList<Locus> locus;
+
+  const LocusMapPage({
+    required this.locus,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.005),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            TopMenuToolsWidget(),
+          ],
+        ),
+      );
+}
