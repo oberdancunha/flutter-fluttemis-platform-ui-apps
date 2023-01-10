361f30fa2b45ad77c8267714f86ba53f936a6ae1
oberdancunha <oberdancunha@gmail.com>
2023-01-10 13:03:23 -0300

    Remove the iOS platform test.

--- fluttemis_platform_ui_design_system/lib/presentation/components/platform/show_dialog/platform_show_dialog.dart
+++ fluttemis_platform_ui_design_system/lib/presentation/components/platform/show_dialog/platform_show_dialog.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
 
@@ -10,13 +9,7 @@ Future<T?> platformShowDialog<T>({
 }) {
   const barrierDismissible = true;
 
-  if (Platform.isIOS) {
-    return showCupertinoDialog(
-      context: context,
-      builder: builder,
-      barrierDismissible: barrierDismissible,
-    );
-  } else if (Platform.isMacOS) {
+  if (Platform.isMacOS) {
     return showMacosSheet(
       context: context,
       barrierDismissible: barrierDismissible,
