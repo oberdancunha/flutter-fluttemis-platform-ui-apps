f3d285e9285ac98e22822f21ae29defafb6a8d1e
oberdancunha <oberdancunha@gmail.com>
2023-01-20 10:14:01 -0300

    Remove PlatformCardWidget widget.

--- fluttemis_platform_ui_design_system/lib/presentation/components/overview_data/overview_data_widget.dart
+++ fluttemis_platform_ui_design_system/lib/presentation/components/overview_data/overview_data_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 
-import '../platform/card/platform_card_widget.dart';
 import '../platform/scrollbar/platform_scrollbar_widget.dart';
 import '../platform/text/platform_text_widget.dart';
 import '../platform/text/text_type_enum.dart';
@@ -43,48 +42,46 @@ class _OverviewDataWidgetState extends State<OverviewDataWidget> {
         builder: (_, constraints) => SizedBox(
           width: constraints.maxWidth,
           height: 60,
-          child: PlatformCardWidget(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Image.asset(
-                    widget.image,
-                    width: 35,
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        PlatformScrollbarWidget(
-                          controller: _controller,
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 2),
-                            child: SingleChildScrollView(
-                              physics: const ClampingScrollPhysics(),
-                              controller: _controller,
-                              scrollDirection: Axis.horizontal,
-                              padding: const EdgeInsets.only(bottom: 6),
-                              child: PlatformTextWidget(
-                                widget.value,
-                                textType: widget.textType ?? TextType.label,
-                                fontSize: 13,
-                              ),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Image.asset(
+                  widget.image,
+                  width: 35,
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      PlatformScrollbarWidget(
+                        controller: _controller,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 2),
+                          child: SingleChildScrollView(
+                            physics: const ClampingScrollPhysics(),
+                            controller: _controller,
+                            scrollDirection: Axis.horizontal,
+                            padding: const EdgeInsets.only(bottom: 6),
+                            child: PlatformTextWidget(
+                              widget.value,
+                              textType: widget.textType ?? TextType.label,
+                              fontSize: 13,
                             ),
                           ),
                         ),
-                        PlatformTextWidget(
-                          widget.description,
-                          textType: TextType.caption,
-                          fontSize: 10,
-                        ),
-                      ],
-                    ),
+                      ),
+                      PlatformTextWidget(
+                        widget.description,
+                        textType: TextType.caption,
+                        fontSize: 10,
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
--- fluttemis_platform_ui_design_system/lib/presentation/components/overview_multiple_data/overview_multiple_data_list_widget.dart
+++ fluttemis_platform_ui_design_system/lib/presentation/components/overview_multiple_data/overview_multiple_data_list_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 
-import '../platform/card/platform_card_widget.dart';
 import '../platform/scrollbar/platform_scrollbar_widget.dart';
 import '../platform/text/platform_text_widget.dart';
 import '../platform/text/text_type_enum.dart';
@@ -37,40 +36,38 @@ class _OverviewMultipleDataListWidgetState extends State<OverviewMultipleDataLis
   @override
   Widget build(BuildContext context) => SizedBox(
         height: 200,
-        child: PlatformCardWidget(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              right: 3,
-              left: 8,
-              top: 8,
-              bottom: 8,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                PlatformTextWidget(
-                  widget.title,
-                  textType: TextType.subTitle,
-                  fontSize: 15,
-                ),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: PlatformScrollbarWidget(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            right: 3,
+            left: 8,
+            top: 8,
+            bottom: 8,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PlatformTextWidget(
+                widget.title,
+                textType: TextType.subTitle,
+                fontSize: 15,
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: PlatformScrollbarWidget(
+                  controller: _scrollController,
+                  child: GridView.count(
+                    crossAxisSpacing: 5,
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 20,
+                    physics: const ClampingScrollPhysics(),
+                    childAspectRatio: 2.23,
                     controller: _scrollController,
-                    child: GridView.count(
-                      crossAxisSpacing: 5,
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 20,
-                      physics: const ClampingScrollPhysics(),
-                      childAspectRatio: 2.23,
-                      controller: _scrollController,
-                      padding: const EdgeInsets.only(right: 6),
-                      children: widget.children,
-                    ),
+                    padding: const EdgeInsets.only(right: 6),
+                    children: widget.children,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       );
