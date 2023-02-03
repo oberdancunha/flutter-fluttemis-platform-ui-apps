f146e1aefd967d4cd9b9a623b0bab3239be5ca6d
oberdancunha <oberdancunha@gmail.com>
2023-02-03 14:54:34 -0300

    Update platforms plugin manager.

--- fluttemis_platform_ui_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ fluttemis_platform_ui_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import macos_ui
 import screen_retriever
+import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
