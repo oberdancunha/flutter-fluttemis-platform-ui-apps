e73a7092a9703b771e145173b5f0fd15155e10d5
oberdancunha <oberdancunha@gmail.com>
2023-02-10 19:10:27 -0300

    Remove unecessary SingleChildScrollView.

--- fluttemis_platform_ui_app/lib/presentation/locus/locus_resume/locus_feature_details_widget.dart
+++ fluttemis_platform_ui_app/lib/presentation/locus/locus_resume/locus_feature_details_widget.dart
@@ -24,80 +24,78 @@ class LocusFeatureDetailsWidget extends StatelessWidget {
         return SizedBox(
           height: constraints.maxHeight,
           width: constraints.maxWidth,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                GridView.count(
-                  crossAxisCount: crossAxisCount > 0 ? crossAxisCount : 1,
-                  childAspectRatio: 2.5,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
+          child: Column(
+            children: [
+              GridView.count(
+                crossAxisCount: crossAxisCount > 0 ? crossAxisCount : 1,
+                childAspectRatio: 2.5,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  OverviewDataWidget(
+                    value: locusFeature.start.toString(),
+                    description: fluttemisAppLocalizations.featureStart,
+                    image: 'assets/images/feature/feature_start.png',
+                    textType: TextType.label,
+                  ),
+                  OverviewDataWidget(
+                    value: locusFeature.end.toString(),
+                    description: fluttemisAppLocalizations.featureEnd,
+                    image: 'assets/images/feature/feature_end.png',
+                    textType: TextType.label,
+                  ),
+                  OverviewDataWidget(
+                    value: locusFeature.strand.label,
+                    description: fluttemisAppLocalizations.featureStrand,
+                    image: 'assets/images/feature/feature_strand.png',
+                    textType: TextType.label,
+                  ),
+                  if (locusFeature.name != null)
                     OverviewDataWidget(
-                      value: locusFeature.start.toString(),
-                      description: fluttemisAppLocalizations.featureStart,
-                      image: 'assets/images/feature/feature_start.png',
+                      value: locusFeature.name!,
+                      description: fluttemisAppLocalizations.featureName,
+                      image: 'assets/images/feature/feature_name.png',
                       textType: TextType.label,
                     ),
-                    OverviewDataWidget(
-                      value: locusFeature.end.toString(),
-                      description: fluttemisAppLocalizations.featureEnd,
-                      image: 'assets/images/feature/feature_end.png',
-                      textType: TextType.label,
-                    ),
-                    OverviewDataWidget(
-                      value: locusFeature.strand.label,
-                      description: fluttemisAppLocalizations.featureStrand,
-                      image: 'assets/images/feature/feature_strand.png',
-                      textType: TextType.label,
-                    ),
-                    if (locusFeature.name != null)
-                      OverviewDataWidget(
-                        value: locusFeature.name!,
-                        description: fluttemisAppLocalizations.featureName,
-                        image: 'assets/images/feature/feature_name.png',
-                        textType: TextType.label,
-                      ),
-                    OverviewDataWidget(
-                      value: locusFeature.type,
-                      description: fluttemisAppLocalizations.featureType,
-                      image: 'assets/images/feature/feature_type.png',
-                      textType: TextType.label,
-                    ),
-                  ],
+                  OverviewDataWidget(
+                    value: locusFeature.type,
+                    description: fluttemisAppLocalizations.featureType,
+                    image: 'assets/images/feature/feature_type.png',
+                    textType: TextType.label,
+                  ),
+                ],
+              ),
+              if (locusFeature.product != null)
+                OverviewDataWidget(
+                  value: locusFeature.product!,
+                  description: fluttemisAppLocalizations.featureProduct,
+                  image: 'assets/images/feature/feature_product.png',
+                  textType: TextType.label,
                 ),
-                if (locusFeature.product != null)
-                  OverviewDataWidget(
-                    value: locusFeature.product!,
-                    description: fluttemisAppLocalizations.featureProduct,
-                    image: 'assets/images/feature/feature_product.png',
-                    textType: TextType.label,
-                  ),
-                if (locusFeature.note != null)
-                  OverviewDataWidget(
-                    value: locusFeature.note!,
-                    description: fluttemisAppLocalizations.featureNote,
-                    image: 'assets/images/feature/feature_note.png',
-                    textType: TextType.label,
-                  ),
-                if (locusFeature.nucleotides != null) ...[
-                  const SizedBox(height: 10),
-                  OverviewDataSequencesWidget(
-                    height: locusFeature.aminoacids != null ? 92 : 295,
-                    title: fluttemisAppLocalizations.nucleotideSequence,
-                    sequences: locusFeature.nucleotides!,
-                  ),
-                ],
-                if (locusFeature.aminoacids != null) ...[
-                  const SizedBox(height: 20),
-                  OverviewDataSequencesWidget(
-                    height: 92,
-                    title: fluttemisAppLocalizations.aminoacidSequence,
-                    sequences: locusFeature.aminoacids!,
-                  ),
-                ],
+              if (locusFeature.note != null)
+                OverviewDataWidget(
+                  value: locusFeature.note!,
+                  description: fluttemisAppLocalizations.featureNote,
+                  image: 'assets/images/feature/feature_note.png',
+                  textType: TextType.label,
+                ),
+              if (locusFeature.nucleotides != null) ...[
+                const SizedBox(height: 10),
+                OverviewDataSequencesWidget(
+                  height: locusFeature.aminoacids != null ? 92 : 295,
+                  title: fluttemisAppLocalizations.nucleotideSequence,
+                  sequences: locusFeature.nucleotides!,
+                ),
               ],
-            ),
+              if (locusFeature.aminoacids != null) ...[
+                const SizedBox(height: 20),
+                OverviewDataSequencesWidget(
+                  height: 92,
+                  title: fluttemisAppLocalizations.aminoacidSequence,
+                  sequences: locusFeature.aminoacids!,
+                ),
+              ],
+            ],
           ),
         );
       },
