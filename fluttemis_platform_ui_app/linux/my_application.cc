622c7630a99c513b86a8f3970732b79a793a65ea
oberdancunha <oberdancunha@gmail.com>
2023-04-11 00:07:16 -0300

    Rebuild platforms to update project name in build.

--- fluttemis_platform_ui_app/linux/my_application.cc
+++ fluttemis_platform_ui_app/linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "fluttemis_platform_ui_app");
+    gtk_header_bar_set_title(header_bar, "Fluttemis");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "fluttemis_platform_ui_app");
+    gtk_window_set_title(window, "Fluttemis");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
