6e583d5030cdce0eac650876d04776581dd84ece
oberdancunha <oberdancunha@gmail.com>
2023-04-24 23:39:06 -0300

    Changes the close button of the dialog window with the new implemented Windows platform button.

--- fluttemis_platform_ui_design_system/lib/presentation/components/platform/close_dialog_button/windows_close_dialog_button_widget.dart
+++ fluttemis_platform_ui_design_system/lib/presentation/components/platform/close_dialog_button/windows_close_dialog_button_widget.dart
@@ -1,7 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
 
-import '../tooltip/windows_tooltip_widget.dart';
+import '../window_button/windows/windows_close_window_button_widget.dart';
 
 class WindowsCloseDialogButtonWidget extends StatelessWidget {
   const WindowsCloseDialogButtonWidget({super.key});
@@ -9,47 +9,9 @@ class WindowsCloseDialogButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Align(
         alignment: Alignment.topRight,
-        child: SizedBox(
-          height: 40,
-          width: 40,
-          child: WindowsTooltipWidget(
-            message: FluttemisAppLocalizations.of(context)!.closeWindow,
-            child: Button(
-              style: ButtonStyle(
-                backgroundColor: ButtonState.resolveWith(
-                  (states) {
-                    if (states.isHovering || states.isFocused) {
-                      return const Color(0XFFDD2622);
-                    }
-
-                    return const Color(0x00000000);
-                  },
-                ),
-                border: ButtonState.all(
-                  const BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                shape: ButtonState.all(
-                  const RoundedRectangleBorder(),
-                ),
-                foregroundColor: ButtonState.resolveWith((states) {
-                  if (states.isHovering || states.isFocused) {
-                    return const Color(0XFFF9DBDD);
-                  }
-
-                  return FluentTheme.of(context).iconTheme.color;
-                }),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Icon(
-                FluentIcons.clear,
-                size: 13,
-              ),
-            ),
-          ),
+        child: WindowsCloseWindowButtonWidget(
+          tooltip: FluttemisAppLocalizations.of(context)!.closeWindow,
+          action: () => Navigator.of(context).pop(),
         ),
       );
 }
