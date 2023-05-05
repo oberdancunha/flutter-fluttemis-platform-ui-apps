2649cd40c1c98c8d4ae855c7fdb0e9fb3effdd4b
oberdancunha <oberdancunha@gmail.com>
2023-05-05 16:51:23 -0300

    Change the LocusDataSource and LocusShowStore bindings from the genbank_file_module file to the locus_module.

--- fluttemis_platform_ui_app/lib/presentation/modules/genbank_file_module.dart
+++ fluttemis_platform_ui_app/lib/presentation/modules/genbank_file_module.dart
@@ -3,28 +3,9 @@ import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_de
 import 'package:fluttemis_platform_ui_locus/application/locus/locus_store.dart';
 import 'package:fluttemis_platform_ui_locus/application/locusShow/locus_show_store.dart';
 import 'package:fluttemis_platform_ui_locus/configurations/locus_module.dart';
-import 'package:fluttemis_platform_ui_locus/external/locus/locus_data_source_genbank_file.dart';
-import 'package:fluttemis_platform_ui_locus/infrastructure/locus/i_locus_data_source.dart';
 import 'package:fluttemis_platform_ui_locus/presentation/locus_widget.dart';
 
 class GenbankFileModule extends Module {
-  @override
-  final List<Bind> binds = [
-    Bind.factory<ILocusDataSource>(
-      (i) {
-        final genbankFile = i.args.queryParams['filepath'].toString();
-
-        return LocusDataSourceGenbankFile(genbankFile: genbankFile);
-      },
-    ),
-    Bind.lazySingleton<LocusShowStore>(
-      (_) => LocusShowStore(),
-      onDispose: (locusShowStore) {
-        locusShowStore.dispose();
-      },
-    ),
-  ];
-
   @override
   List<Module> get imports => [
         LocusModule(),
--- fluttemis_platform_ui_locus/lib/configurations/locus_module.dart
+++ fluttemis_platform_ui_locus/lib/configurations/locus_module.dart
@@ -1,7 +1,9 @@
 import 'package:fluttemis_platform_ui_dependency_module/fluttemis_platform_ui_dependency_module.dart';
 
 import '../application/locus/locus_store.dart';
+import '../application/locusShow/locus_show_store.dart';
 import '../domain/locus/i_locus_repository.dart';
+import '../external/locus/locus_data_source_genbank_file.dart';
 import '../infrastructure/locus/i_locus_data_source.dart';
 import '../infrastructure/locus/locus_overlap.dart';
 import '../infrastructure/locus/locus_repository.dart';
@@ -9,6 +11,14 @@ import '../infrastructure/locus/locus_repository.dart';
 class LocusModule extends Module {
   @override
   List<Bind<Object>> get binds => [
+        Bind.factory<ILocusDataSource>(
+          (i) {
+            final genbankFile = i.args.queryParams['filepath'].toString();
+
+            return LocusDataSourceGenbankFile(genbankFile: genbankFile);
+          },
+          export: true,
+        ),
         Bind.factory<ILocusRepository>(
           (i) => LocusRepository(
             locusDataSource: i<ILocusDataSource>(),
@@ -18,6 +28,13 @@ class LocusModule extends Module {
         ),
         Bind.factory<LocusStore>(
           (i) => LocusStore(locusRepository: i<ILocusRepository>()),
+          export: true,
+        ),
+        Bind.lazySingleton<LocusShowStore>(
+          (_) => LocusShowStore(),
+          onDispose: (locusShowStore) {
+            locusShowStore.dispose();
+          },
           export: true,
         ),
       ];
